d19c677b7a11624ab527a2e1fb056929f5dab511
Jyoti Singh <singhjyoti010@outlook.com>
2024-08-05 00:45:43 +0530

    added credentials privacy and updated .gitignore to have .env

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env.loginUsername = process.env.USER_NAME;
+      config.env.loginPassword = process.env.USER_PASSWORD;
+      config.env.loginEmail = process.env.USER_EMAIL;
+      return config;
     },
     baseUrl: "https://x.com/",
   },
