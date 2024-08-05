3f70e86d7f1a119ed53b677b4d48c52505a7030d
Jyoti Singh <singhjyoti010@outlook.com>
2024-08-05 13:22:27 +0530

    added commentsto loginPage-hooks.js, updated login-helper to import selctors from loginhooks

--- cypress/e2e/hooks/loginPageHook.js
+++ cypress/e2e/hooks/loginPageHook.js
@@ -1,11 +1,11 @@
 export const loginPageHooks = {
-    XMigrationBottomBtn : '[data-testid="xMigrationBottomBar"]',    
-    signinBtn: '[data-testid="loginButton"]',
-    loginDialog: '.r-1roi411 > :nth-child(1) > .r-16y2uox',
-    emailInput: 'input[autocomplete="username"]',
-    nextBtn: '//*[text()="Next"]//ancestor::button',
-    confirmAccountPopUp: '[data-testid="ocfEnterTextTextInput"]',
-    userNameInput: '[data-testid="ocfEnterTextTextInput"]',
-    passwordInput: '//*[contains(@name,"password")]',
-    loginBtn: 'button[data-testid="LoginForm_Login_Button"]'
+    XMigrationBottomBtn : '[data-testid="xMigrationBottomBar"]',        //welcome notification 'X' button
+    signinBtn: '[data-testid="loginButton"]',       //signin Button
+    loginDialog: '.r-1roi411 > :nth-child(1) > .r-16y2uox',     //login / signin dialog
+    emailInput: 'input[autocomplete="username"]',       //email, phone or username text field
+    nextBtn: '//*[text()="Next"]//ancestor::button',        //Next button
+    confirmAccountPopUp: '[data-testid="ocfEnterTextTextInput"]',       //Intermittent, account confirmation pop-out
+    userNameInput: '[data-testid="ocfEnterTextTextInput"]',     //username text field
+    passwordInput: '//*[contains(@name,"password")]',       //passwrod text field
+    loginBtn: 'button[data-testid="LoginForm_Login_Button"]'        //login button
 }
