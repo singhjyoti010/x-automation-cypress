bc7b320cceb2f45573c0dd8127c4a5104cc16f52
Jyoti Singh <singhjyoti010@outlook.com>
2024-08-11 21:42:14 +0530

    added TS support

--- cypress/e2e/page-objects/login-helper.js
+++ cypress/e2e/page-objects/login-helper.js
@@ -2,17 +2,16 @@ import { loginPageHooks } from "../hooks/loginPageHook";
 import { click, type } from "./event-helpers";
 
 export class LoginPage {
+    //****************************************login methods*****************************************
     async login() {
-
-        //****************************************login methods*****************************************
-
         await cy.visit('/');
         await click(loginPageHooks.XMigrationBottomBtn);
         await click(loginPageHooks.signinBtn);
         await cy.get(loginPageHooks.loginDialog, {timeout: 7000}).should('be.visible'); //waits for login dialog to show up
         await type(loginPageHooks.emailInput, Cypress.env('loginEmail'));
         await cy.xpath(loginPageHooks.nextBtn).click();
-        if( await cy.get(loginPageHooks.confirmAccountPopUp).then(($el) => {
+        if( await cy.get(loginPageHooks.confirmAccountPopUp)
+                    .then(($el) => {
             Cypress.dom.isVisible($el) // true
           })){
             await type(loginPageHooks.userNameInput, Cypress.env('loginUsername'));
