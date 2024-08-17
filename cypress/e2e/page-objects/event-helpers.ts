0ffc7e8a6f1a3354d73beee8cabdab5b7d9c81fc
Jyoti Singh <singhjyoti010@outlook.com>
2024-08-17 22:00:19 +0530

    updated page-objects to support .ts

--- cypress/e2e/page-objects/event-helpers.ts
+++ cypress/e2e/page-objects/event-helpers.ts
@@ -1,6 +1,6 @@
 export async function click (selector, timeout = 10000) {
     try{
-        await cy.get(selector, {timeout: timeout}).should('be.visible').click();
+        cy.get(selector, {timeout: timeout}).should('be.visible').click();
     } catch (e) {
         throw new Error(`Element ${selector} was not clickable after ${timeout} ms : ${e}`);
     }
@@ -8,7 +8,7 @@ export async function click (selector, timeout = 10000) {
 
 export async function type(selector, text, timeout = 10000) {
     try {
-        await cy.get(selector, {timeout: timeout}).should('be.visible').type(text);
+        cy.get(selector, {timeout: timeout}).should('be.visible').type(text);
     } catch(e) {
         throw new Error(`Failed to type ${text} on element ${selector} in ${timeout} ms : ${e}`);
     }
